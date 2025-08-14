35feddf60f8bfaea57491cc0e787a105d0d1b51f
Sirus Snitch <chedli.bousbih@gmail.com>
2025-08-14 16:36:20 +0100

    FireNetInit

--- Start.bat
+++ Start.bat
@@ -4,13 +4,13 @@ REM Activate virtual environment
 call venv\Scripts\activate
 
 REM Start Uvicorn in a new terminal
-start cmd /k "call venv\Scripts\activate && uvicorn smartVision.asgi:application --reload"
+start cmd /k "call venv311\Scripts\activate && uvicorn smartVision.asgi:application --reload"
 
 REM Start Celery beat in a new terminal
-start cmd /k "call venv\Scripts\activate && celery -A smartVision beat -l info"
+start cmd /k "call venv311\Scripts\activate && celery -A smartVision beat -l info"
 
 REM Start Celery worker in a new terminal
-start cmd /k "call venv\Scripts\activate && celery -A smartVision worker -l info"
+start cmd /k "call venv311\Scripts\activate && celery -A smartVision worker -l info"
 
 echo All services started.
 pause
